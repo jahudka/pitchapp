e64569b700be7fba2e632763037e27a69818aa54
Eliška Kaderová <me@subsonic.cz>
2026-03-15 19:44:11 +0100

    init commit

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,7 @@ const config = {
       fallback: 'index.html',
     }),
     paths: {
-      base: process.env.BASE_PATH,
+      base: process.env.BASE_PATH?.replace(/\/$/, ''),
       relative: false,
     },
     csp: {
